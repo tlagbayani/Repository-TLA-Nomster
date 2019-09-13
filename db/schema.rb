5c74d3de3e205b8ed0db9c824075b5fe68b40d10
tlagbayani <tlagbayani@gmail.com>
2019-09-13 02:48:46 +0000

    promote schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+# dummy comment
 
 ActiveRecord::Schema.define(version: 2019_08_22_003802) do
 
@@ -31,6 +32,11 @@ ActiveRecord::Schema.define(version: 2019_08_22_003802) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
