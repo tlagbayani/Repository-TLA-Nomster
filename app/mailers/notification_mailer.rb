c3ae4786f3beaf20f52fcaa9be4e91abdd8d8e12
tlagbayani <tlagbayani@gmail.com>
2019-09-27 00:54:40 +0000

    Applied changes for Lesson 41 Previewing the email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "nomster.test01@gmail.com",
+    mail(to: "nomster.user01@gmail.com",
          subject: "A comment has been added to your place")
   end 
 end
